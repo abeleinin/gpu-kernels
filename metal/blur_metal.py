a9003cddbc94898e6db987b138f9441f828d5d59
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-10-28 23:58:40 -0500

    blur metal kernel

---
+++ metal/blur_metal.py
@@ -0,0 +1,93 @@
+import os
+import sys
+
+from PIL import Image
+
+import numpy as np
+import mlx.core as mx
+
+
+def blur_kernel(a: mx.array):
+    header = """
+        constant uint BLUR_SIZE = 1;
+        constant uint CHANNELS = 3;
+    """
+
+    source = """
+        uint col = threadgroup_position_in_grid.x * \
+                   threads_per_threadgroup.x + \
+                   thread_position_in_threadgroup.x;
+        uint row = threadgroup_position_in_grid.y * \
+                   threads_per_threadgroup.y + \
+                   thread_position_in_threadgroup.y;
+
+        if (col < a_shape[0] && row < a_shape[1]) {
+            int pixValR = 0;
+            int pixValG = 0;
+            int pixValB = 0;
+
+            int pixels = 0;
+
+            for (int blurRow=-BLUR_SIZE; blurRow < BLUR_SIZE+1; ++blurRow) {
+                for (int blurCol=-BLUR_SIZE; blurCol < BLUR_SIZE+1; ++blurCol) {
+                    uint currRow = row + blurRow;
+                    uint currCol = col + blurCol;
+
+                    if (currRow >= 0 && currRow < a_shape[1] && currCol >= 0 && currCol < a_shape[0]) {
+                        uint offset = currRow*a_shape[0] + currCol;
+                        uint rgbOffset = offset*CHANNELS;
+                        pixValR += a[rgbOffset    ];
+                        pixValG += a[rgbOffset + 1];
+                        pixValB += a[rgbOffset + 2];
+                        ++pixels;
+                    }
+                }
+            }
+
+            uint offset = (row*a_shape[0] + col) * CHANNELS;
+            out[offset    ] = (unsigned char)(pixValR/pixels);
+            out[offset + 1] = (unsigned char)(pixValG/pixels);
+            out[offset + 2] = (unsigned char)(pixValB/pixels);
+        }
+    """
+
+    kernel = mx.fast.metal_kernel(
+        name="blur",
+        input_names=["a"],
+        output_names=["out"],
+        header=header,
+        source=source,
+    )
+
+    output = kernel(
+        inputs=[a],
+        grid=(a.shape[0], a.shape[1], 1),
+        threadgroup=(8, 8, 1),
+        output_shapes=[a.shape],
+        output_dtypes=[mx.float32],
+        stream=mx.gpu,
+        init_value=0
+    )
+
+    return output[0]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: blur_metal.py path/to/img.jpg")
+        sys.exit(1)
+
+    # open image
+    img_path = sys.argv[1]
+    img = Image.open(img_path)
+    img = mx.array(np.array(img))
+
+    # call metal kernel
+    blur_img = blur_kernel(img)
+
+    # convert back to uint8 PIL.Image and save as jpg
+    blur_img_uint8 = (np.array(blur_img)).astype('uint8')
+    image = Image.fromarray(blur_img_uint8)
+    input_name, _ = os.path.splitext(os.path.basename(img_path))
+    image.save(f'blur_{input_name}.jpg', format='JPEG')
+
